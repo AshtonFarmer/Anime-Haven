373a7cdc253237d3ee5981c2fee9c8d99c316746
Ashton Farmer <ashton20.bama@gmail.com>
2026-07-22 18:10:47 -0500

    Refresh Anime Haven offline cache for v6

--- sw-v2.js
+++ sw-v2.js
@@ -1,5 +1,23 @@
-const CACHE='anime-haven-v3';
-const CORE=["./", "./index.html", "./styles-v2.css", "./data-a.js", "./data-b.js", "./data-c.js", "./data-d.js", "./config-v2.js", "./app-v2.js", "./manifest-v2.webmanifest", "./icons/icon.svg"];
+const CACHE='anime-haven-v6';
+const CORE=[
+  './',
+  './index.html',
+  './bootstrap-v3.js?v=6',
+  './manifest-v2.webmanifest',
+  './icons/icon.svg',
+  './anime-haven-v4.part00?v=4',
+  './anime-haven-v4.part01?v=4',
+  './anime-haven-v4.part02?v=4'
+];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(
+    caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
