8d3b6ea9d14b47df66ce25aee0c93cf4fe9f5db1
Ashton Farmer <ashton20.bama@gmail.com>
2026-07-23 02:18:10 -0500

    Refresh cache for crash fix and install assets

--- sw-v2.js
+++ sw-v2.js
@@ -1,23 +1,28 @@
-const CACHE='anime-haven-v15';
+const CACHE='anime-haven-v16';
 const CORE=[
   './',
   './index.html',
-  './bootstrap-v4.js?release=10',
+  './bootstrap-v4.js?release=16',
   './rubber-search-v11.js?release=11',
   './rubber-search-v11.css?release=11',
   './rubber-search-compact-v12.js?release=12',
   './rubber-search-compact-v12.css?release=12',
   './nav-scroll-guard-v13.js?release=14',
-  './library-manager-v14.js?release=14',
-  './library-manager-v14.css?release=14',
+  './library-manager-v15.js?release=15',
+  './library-manager-v14.css?release=15',
   './manifest-v2.webmanifest',
+  './manifest.webmanifest',
   './icons/icon.svg',
   './anime-haven-v4.part00?package=4',
   './anime-haven-v4.part01?package=4',
   './anime-haven-v4.part02?package=4'
 ];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('install',event=>event.waitUntil(
+  caches.open(CACHE).then(cache=>Promise.all(CORE.map(url=>cache.add(url).catch(error=>console.warn('Anime Haven cache skipped',url,error))))).then(()=>self.skipWaiting())
+));
+self.addEventListener('activate',event=>event.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
   event.respondWith(fetch(event.request).then(response=>{
